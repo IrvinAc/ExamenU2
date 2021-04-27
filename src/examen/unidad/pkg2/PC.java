2895f70154629304249f89443fc0a247e706eacd
Irvin <Irvin@DESKTOP-KL6RO2S>
2021-04-27 15:33:23 -0600

    Intento 1

--- src/examen/unidad/pkg2/PC.java
+++ src/examen/unidad/pkg2/PC.java
@@ -20,23 +20,23 @@ public class PC {
         System.out.println("Tiene " + ram + "gb de ram");
         System.out.println("Tiene un disco " + disk );
 }
-    public void getencender (){
+    public void encender (){
         System.out.println("Enciendes la pc");
     }
-    public void getencender (int ent){
+    public void encender (int ent){
         System.out.println("La pc tarda en encender " + ent + "min.");
     }
-    public void getencender (int ent, String disk){
+    public void encender (int ent, String disk){
         System.out.println("La pc enciende en " + ent + "min, por el disco " + disk + " que tiene");
     }
-    public void getespacio (double espacio){
+    public void espacio (double espacio){
         if (espacio>=1000){
             double TB=espacio/1000;
             System.out.println("La pc tiene " + TB + "Tb de espacio ");
         }else
         System.out.println("la pc tiene " + espacio + "GB de espacio");
     }
-    public void getespacio (int espacio, int part ){
+    public void espacio (int espacio, int part ){
         System.out.println("El disco tiene una particion de " + part+ "GB ");
         double espct=espacio-part;
         if (espct>=1000){
